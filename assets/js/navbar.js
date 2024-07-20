72caacc26e4524cd7eafb58c450809e04a1d91da
Cmoon30 <movers.garcia@gmail.com>
2024-07-20 15:28:39 +0800

    add navigation tab function and finished homepage

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,21 @@
 export const navLinks = () => {
 	const navLinks = $(".navlinks").children();
+	const pageContainers = $(".pageContainer");
+
+	let defaultIndex = 0;
+
+	const navTab = (index) => {
+		pageContainers.removeClass("activePage");
+		$(pageContainers[index]).addClass("activePage");
+
+		navLinks.removeClass("active");
+		$(navLinks[index]).addClass("active");
+	};
 
 	navLinks.on("click", (event) => {
-		navLinks.removeClass("active");
-		$(event.target).addClass("active");
+		const index = $(event.target).index();
+		navTab(index);
 	});
+
+	navTab(defaultIndex);
 };
